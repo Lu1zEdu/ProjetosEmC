409121daf18c959ec0d0ad3a32db286a55447abf
Lu1zEdu <le6269199@gmail.com>
2024-09-16 20:34:06 -0300

    Hello_World

---
+++ Hello_World.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  // Inclui a biblioteca padrão de entrada e saída
+
+int main() {
+    printf("Hello, World!\n");  // Imprime a mensagem na tela
+    return 0;  // Retorna 0 indicando que o programa terminou com sucesso
+}
