256f2b1951898d83d622bdfb7b0fd500a607e08b
Lu1zEdu <le6269199@gmail.com>
2024-09-17 14:17:54 -0300

    If-Else

---
+++ if,else if,else/If.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 10;
+
+    if (numero > 0)
+    {
+        printf("O número é positivo.\n");
+    }
+    else if (numero < 0)
+    {
+        printf("O número é negativo.\n");
+    }
+    else
+    {
+        printf("O número é zero.\n");
+    }
+
+    return 0;
+}
